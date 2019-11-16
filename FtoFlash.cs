78c7728ba14a026013a0cc7769af20fb0d91a42a
ShaunCuthbert <55137476+ShaunCuthbert@users.noreply.github.com>
2019-11-16 22:09:39 +0000

    Flashlight now with batteries

--- FtoFlash.cs
+++ FtoFlash.cs
@@ -3,19 +3,43 @@ using System.Collections;
 
 public class FtoFlash : MonoBehaviour
 {
-    private Light myLight;
-
+    private bool flashlightEnabled;
+    public GameObject flashlight;
+    public GameObject lightObj;
+    public float maxEnergy;
+    public float currentEnergy;
 
     void Start()
     {
-        myLight = GetComponent<Light>();
+        currentEnergy = maxEnergy;
+        maxEnergy = 100;
+        currentEnergy = maxEnergy;
     }
 
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.F))
         {
-            myLight.enabled = !myLight.enabled;
+            flashlightEnabled =! flashlightEnabled;
+        }
+        if (flashlightEnabled)
+        {
+            flashlight.SetActive(true);
+            currentEnergy -= 0.5f * Time.deltaTime;
+            if (currentEnergy <= 0)
+            {
+                lightObj.SetActive(false);
+
+            }
+        }else
+        {
+            flashlight.SetActive(false);
+            currentEnergy += 1f * Time.deltaTime;
+            
+        }
+        if (currentEnergy > maxEnergy)
+        {
+            currentEnergy = maxEnergy;
         }
     }
 }
