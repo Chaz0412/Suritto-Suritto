d72de10565d991120d7f0f0b9aec0a14be18dbca
ShaunCuthbert <55137476+ShaunCuthbert@users.noreply.github.com>
2019-11-17 14:54:34 +0000

    LevelChanger

---
+++ LevelChanger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    private int leveltoLoad;
+
+    public void FadeToLevel (int levelIndex)
+    {
+        leveltoLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(leveltoLoad);
+    }
+}
