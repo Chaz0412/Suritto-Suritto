18b2df5393a115d428827be8b500b8a232c34683
ShaunCuthbert <55137476+ShaunCuthbert@users.noreply.github.com>
2019-11-18 14:42:23 +0000

    GameManagerV1.0

---
+++ Game Manager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Animator animator;
+    private int leveltoLoad;
+
+    public void FadeToLevel (int levelIndex)
+    {
+        leveltoLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(leveltoLoad);
+    }
+
+    public void loadNextLevel(int x)
+    {
+        SceneManager.LoadScene(x);
+    }
+}
